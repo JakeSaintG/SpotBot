044e2a1a5c9ef8eafcdad84b4f5d48b075999ede
JakeSaintG <jakestgermain@outlook.com>
2021-08-17 00:49:33 -0400

    rework before adding commands

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import * as Discord from 'discord.js';
-import { token } from './token';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
+//Client extends BaseClient extends EventEmitter
 const client = new Discord.Client();
 
-client.once('ready', () => {
-    console.log('SpotBot is online!')
+//bot picks up its own login event and logs it to the console.
+client.on('ready', () => {
+    console.log(`${client.user.username} has logged in.`)
 });
 
-//Has to be at last line of file
-client.login(token);
+client.on('message', (message) => {
+    //console.log(`[${message.author.tag}]: ${message.content}`)
+    if (message.content === "nerd") {
+        console.log(`${message.author.username} has hurted mai feewings...`)
+    }
+})
+
+client.login(process.env.SPOTBOT_TOKEN);
