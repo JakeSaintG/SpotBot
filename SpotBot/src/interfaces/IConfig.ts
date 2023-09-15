51a9ac10d8038bdf8c7cc7d52b50dcb82e4709dd
Jake St. Germain <jakesaint27@gmail.com>
2023-09-14 23:19:30 -0400

    Updated configuration to allow for Spotbot category

--- SpotBot/src/interfaces/IConfig.ts
+++ SpotBot/src/interfaces/IConfig.ts
@@ -3,6 +3,7 @@ export interface IConfig {
     config_last_modified_dts: Date,
     notify_on_server_leave: boolean,
     configured_for_pkmn_go: boolean,
+    spotbot_category_id: null|string,
     channels: any,
     roles: any
 } 
