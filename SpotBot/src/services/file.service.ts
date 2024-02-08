3aaaf213378ae79505f126429d1126ff6daeecc9
JakeSaintG <jakesaint27@gmail.com>
2024-02-08 17:02:02 -0500

    Move templates; file-exist checks now in fileSrv

--- SpotBot/src/services/file.service.ts
+++ SpotBot/src/services/file.service.ts
@@ -1,14 +1,29 @@
-import { autoInjectable } from "tsyringe"
+import { autoInjectable } from "tsyringe";
+import { LogService } from "./log.service";
+const fs = require('fs');;
 
 @autoInjectable()
-class FileService {
-    private fileName: string
+export class FileService {
+    private logger: LogService;
 
-    public constructor(name: string) {
-        this.fileName = name
+    constructor(logger: LogService) {
+        this.logger = logger;
     }
 
-    public getFileName(): string {
-        return this.fileName
+    public ensureTemplatedJsonFileExists = (fileName: string, forceSetup: boolean): void => {
+        this.ensureDirectoryExists('./bot_files', false);
+
+        if (!fs.existsSync(`./bot_files/${fileName}.json`) || forceSetup) {
+            const fileTemplate = JSON.parse(fs.readFileSync(`./src/template_files/${fileName}_template.json`, 'utf8'));
+            console.log(`Creating or recreating file, ${fileName}, from template.`);
+            fs.writeFileSync(`./bot_files/${fileName}.json`, JSON.stringify(fileTemplate, null, 2));
+        }
+    }
+
+    private ensureDirectoryExists = (path: string, forceSetup: boolean): void => {
+        if (!fs.existsSync(path) || forceSetup) {
+            console.log(`Creating directory at ${path}`);
+            fs.mkdirSync(path);
+        }
     }
 }
