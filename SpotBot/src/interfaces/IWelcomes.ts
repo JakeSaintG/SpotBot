5ffd07d852af4ae55e02f900cd630d18430eef6e
JakeSaintG <jakesaint27@gmail.com>
2024-02-17 00:59:41 -0500

    Update welcome template

---
+++ SpotBot/src/interfaces/IWelcomes.ts
@@ -0,0 +1,6 @@
+export interface IWelcomes {
+    default_channel_welcome_message: string,
+    custom_channel_welcome_message: string | undefined | null,
+    default_dm_welcome_message: string,
+    custom_dm_welcome_message: string | undefined | null
+} 
