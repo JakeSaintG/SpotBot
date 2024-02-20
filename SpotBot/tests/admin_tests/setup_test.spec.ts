d114491e64f661cc9bca59966adb4b976772c97c
Jake St. Germain <jakesaint27@gmail.com>
2024-02-19 23:14:32 -0500

    Remove old, not working tests

--- SpotBot/tests/admin_tests/setup_test.spec.ts
+++
@@ -1,9 +0,0 @@
-import { testFn } from '../../src/admin/contestCommands'
-import { assert } from 'chai'
-
-//Temporary; For set up of Mocha/Chai
-describe('testFn', function () {
-    it('should return message received affirmation', function () {
-        assert.strictEqual(testFn('message'), 'message was received')
-    })
-})
