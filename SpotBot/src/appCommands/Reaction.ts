17698cd602612291478a33504d54525b35825bdd
JakeSaintG <jakesaint27@gmail.com>
2024-10-24 13:47:46 -0400

    contined work on reaction command

--- SpotBot/src/appCommands/Reaction.ts
+++ SpotBot/src/appCommands/Reaction.ts
@@ -6,6 +6,10 @@ export const getData = () => {
     return builder
         .setName('reaction')
         .setDescription('Replies with current online status of SpotBot.')
+        .addStringOption(option =>
+			option
+				.setName('emoji')
+				.setDescription('Emoji to react with, separated by spaces.'))
         .addBooleanOption(option =>
             option
                 .setName('help')
@@ -22,29 +26,25 @@ export default class ReactionCommand {
     }
 
     public execute = async (): Promise<void> => {
-        
-        console.log('reaction');
-
         if (!this.interaction.isCommand()) return;
 
-        await this.interaction.reply('SpotBot is online!');
+        const reactionData = this.interaction.options.get('emoji');
+        const reactions = reactionData.value;
+
+        //TODO: validate emoji
+        
+        console.log('SpotBot is online!' + reactions);
+
+        await this.interaction.reply({ content: `I will react with [${reactions}] on the next message you react to! \r\nKeep in mind that I only have access to basic emoji and custom ones from this server.\r\nThis will timeout in one minute.`, ephemeral: true });
+
+
+        //
     };
 
 
     private help = (): string => {
         let helpMsg = `Certainly!\r\n` +
-        `The \`set_welcome\` command allows admins to set the welcome message that SpotBot uses when when a new member joins the server.\r\n` +
-        `Besides \`help\`, the command has an OPTIONAL \`message\` option.\r\n` +
-        `- If text is supplied via this option, that text will be used for the welcome message.\r\n` +
-        `  - Note! The slash command options don't allow for emoji, line breaks, channel links, etc.\r\n` +
-        `  - Only use this option if you want a really simple welcome message.\r\n` + 
-        `- To get a little :sparkles:fancier:sparkles: with your welcome message, leave the \`message\` option **BLANK**.\r\n` + 
-        `  - SpotBot will then prompt you to supply a welcome message and your next message to the channel will be used.\r\n` + 
-        `  - SpotBot will only wait 5min for a response so it may be good to write it out ahead of time!\r\n` + 
-        `- Important notes!\r\n` +
-        `   - SpotBot can only send messages that are less than 2000 characters.\r\n` +
-        `   - If you use server-specific emoji, make sure to update the welcome message.\r\n` +
-        `   - If you have Discord Nitro, make sure to use emoji that SpotBot has access too like general ones and server-specific ones.\r\n\r\n` + 
+        `Work in progress\r\n\r\n` + 
         `Hope this helps!` 
         
         return helpMsg;
--- SpotBot/src/appCommands/Set-Welcome.ts
+++ SpotBot/src/appCommands/Set-Welcome.ts
@@ -6,15 +6,19 @@ export const getData = () => {
     const builder = new SlashCommandBuilder();
     builder
         .setName('set_welcome')
-        .setDescription('Allows an admin to set server-specific welcome messages.')
-        .addBooleanOption(option =>
+        .setDescription(
+            'Allows an admin to set server-specific welcome messages.'
+        )
+        .addBooleanOption((option) =>
             option
                 .setName('help')
-                .setDescription('For help and tips with this command.'))
-		.addStringOption(option =>
-			option
-				.setName('message')
-				.setDescription('OPTIONAL! For simple simple message.'))
+                .setDescription('For help and tips with this command.')
+        )
+        .addStringOption((option) =>
+            option
+                .setName('message')
+                .setDescription('OPTIONAL! For simple simple message.')
+        )
         .setDefaultMemberPermissions(0)
         .setDMPermission(false);
 
@@ -31,7 +35,6 @@ export default class SetWelcomeCommand {
     }
 
     public execute = async (): Promise<void> => {
-        
         if (!this.interaction.isCommand()) return;
 
         let response = 'Setting...';
@@ -39,41 +42,44 @@ export default class SetWelcomeCommand {
         const simpleMessage = this.interaction.options.get('message');
 
         if (this.interaction.options.get('help') && this.interaction.options.get('help').value) {
-            await this.interaction.reply( this.help() );
+            await this.interaction.reply(this.help());
             return;
         }
 
         if (this.interaction.options.get('message') === null) {
-            response = `Please supply the message you would like to use when welcoming new members.\r\n`+
-            `The next message entered by the command issuer will be saved as the welcome message.`;
+            response =
+                `Please supply the message you would like to use when welcoming new members.\r\n` +
+                `The next message entered by the command issuer will be saved as the welcome message.`;
         }
 
-        await this.interaction.reply(
-            response
+        await this.interaction.reply(response);
+
+        let setResponse = await this.services.welcomeService.setWelcomeMessage(
+            this.interaction.channel,
+            this.interaction.user,
+            simpleMessage
         );
-        
-        let setResponse = await this.services.welcomeService.setWelcomeMessage(this.interaction.channel, this.interaction.user, simpleMessage);
 
         await this.interaction.channel.send(setResponse);
     };
 
-
     private help = (): string => {
-        let helpMsg = `Certainly!\r\n` +
-        `The \`set_welcome\` command allows admins to set the welcome message that SpotBot uses when when a new member joins the server.\r\n` +
-        `Besides \`help\`, the command has an OPTIONAL \`message\` option.\r\n` +
-        `- If text is supplied via this option, that text will be used for the welcome message.\r\n` +
-        `  - Note! The slash command options don't allow for emoji, line breaks, channel links, etc.\r\n` +
-        `  - Only use this option if you want a really simple welcome message.\r\n` + 
-        `- To get a little :sparkles:fancier:sparkles: with your welcome message, leave the \`message\` option **BLANK**.\r\n` + 
-        `  - SpotBot will then prompt you to supply a welcome message and your next message to the channel will be used.\r\n` + 
-        `  - SpotBot will only wait 5min for a response so it may be good to write it out ahead of time!\r\n` + 
-        `- Important notes!\r\n` +
-        `   - SpotBot can only send messages that are less than 2000 characters.\r\n` +
-        `   - If you use server-specific emoji, make sure to update the welcome message.\r\n` +
-        `   - If you have Discord Nitro, make sure to use emoji that SpotBot has access too like general ones and server-specific ones.\r\n\r\n` + 
-        `Hope this helps!` 
-        
+        let helpMsg =
+            `Certainly!\r\n` +
+            `The \`set_welcome\` command allows admins to set the welcome message that SpotBot uses when when a new member joins the server.\r\n` +
+            `Besides \`help\`, the command has an OPTIONAL \`message\` option.\r\n` +
+            `- If text is supplied via this option, that text will be used for the welcome message.\r\n` +
+            `  - Note! The slash command options don't allow for emoji, line breaks, channel links, etc.\r\n` +
+            `  - Only use this option if you want a really simple welcome message.\r\n` +
+            `- To get a little :sparkles:fancier:sparkles: with your welcome message, leave the \`message\` option **BLANK**.\r\n` +
+            `  - SpotBot will then prompt you to supply a welcome message and your next message to the channel will be used.\r\n` +
+            `  - SpotBot will only wait 5min for a response so it may be good to write it out ahead of time!\r\n` +
+            `- Important notes!\r\n` +
+            `   - SpotBot can only send messages that are less than 2000 characters.\r\n` +
+            `   - If you use server-specific emoji, make sure to update the welcome message.\r\n` +
+            `   - If you have Discord Nitro, make sure to use emoji that SpotBot has access too like general ones and server-specific ones.\r\n\r\n` +
+            `Hope this helps!`;
+
         return helpMsg;
-    }
+    };
 }
