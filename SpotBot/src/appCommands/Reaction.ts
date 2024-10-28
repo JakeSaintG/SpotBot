10af2191580a041aafeaf17a81356541f723991c
JakeSaintG <jakesaint27@gmail.com>
2024-10-27 22:41:38 -0400

    get very basic emoting happening

--- SpotBot/src/appCommands/Reaction.ts
+++ SpotBot/src/appCommands/Reaction.ts
@@ -1,4 +1,4 @@
-import { Interaction } from 'discord.js';
+import { Guild, GuildMember, Interaction, Message, TextChannel, User } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 
 export const getData = () => {
@@ -9,13 +9,18 @@ export const getData = () => {
         .addStringOption(option =>
 			option
 				.setName('emoji')
-				.setDescription('Emoji to react with, separated by spaces.'))
+				.setDescription('Emoji to react with, separated by spaces.')
+            .setRequired(true))
+        .addStringOption(option =>
+            option
+                .setName('message-url')
+                .setDescription('Message URL to react to.')
+                .setRequired(true))
         .addBooleanOption(option =>
             option
                 .setName('help')
                 .setDescription('For help and tips with this command.'))
-        .setDefaultMemberPermissions(0)
-        .setDMPermission(false);
+        .setDefaultMemberPermissions(0);
 };
 
 export default class ReactionCommand {
@@ -29,14 +34,40 @@ export default class ReactionCommand {
         if (!this.interaction.isCommand()) return;
 
         const reactionData = this.interaction.options.get('emoji');
+        const reactionUrl: string = this.interaction.options.get('message-url').value.toString();
         const reactions = reactionData.value;
 
         //TODO: validate emoji
+
+        if (!reactionUrl.includes('https://discord.com/channels/')) {
+            this.interaction.reply({ 
+                content: `No valid message URL received`, 
+                ephemeral: true
+            });
+
+            return;
+        }
+
+        const ids: string[] = reactionUrl.split('https://discord.com/channels/')[1].split('/');
+        const channelID = ids[1];
+        const messageID = ids[2];
+
+        // TODO; This feels fragile...
+        const channel = this.interaction.guild.channels.cache.find((channel: TextChannel) => channel.id === channelID) as TextChannel;
+
+        const message = await channel.messages.fetch(messageID);
         
-        console.log('SpotBot is online!' + reactions);
+        // TODO: loop and react to each in array
+        message.react(reactions.toString());
 
-        await this.interaction.reply({ content: `I will react with [${reactions}] on the next message you react to! \r\nKeep in mind that I only have access to basic emoji and custom ones from this server.\r\nThis will timeout in one minute.`, ephemeral: true });
+        await this.interaction.reply({ 
+            content: `I will react with [${reactions}]`, 
+            ephemeral: true
+        });
 
+
+
+        // this.interaction.user.accentColor
 
         //
     };
